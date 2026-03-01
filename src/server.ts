de836f5c2c6fce50a11578d3a77d02abcbc18b14
andersonkaiti <anderkaiti@gmail.com>
2026-03-01 01:00:58 -0300

    :sparkles: feat(fastify): integrate fastify and expose server port

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+import Fastify from 'fastify'
+import { env } from './config/env.js'
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.get('/', async function handler(request, reply) {
+  return { hello: 'world' }
+})
+
+try {
+  await fastify.listen({ port: env.PORT }, () => {
+    console.log(`Server running at http://localhost:${env.PORT}`)
+  })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
