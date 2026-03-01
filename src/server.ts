07ac7037c14c256cbad48f8144334e4d2db0938d
andersonkaiti <anderkaiti@gmail.com>
2026-03-01 01:22:27 -0300

    :sparkles: feat(swagger): expose automatic openapi documentation using swagger and scalar ui

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import fastifySwagger from '@fastify/swagger'
+import scalarApiReference from '@scalar/fastify-api-reference'
 import Fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
@@ -13,6 +16,30 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Bootcamp Treinos API',
+      description: 'API para gerenciamento de treinos',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        description: 'Localhost',
+        url: `http://localhost:${env.PORT}`,
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+})
+
+await app.register(scalarApiReference, {
+  routePrefix: '/docs',
+  configuration: {
+    theme: 'kepler',
+  },
+})
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: 'GET',
