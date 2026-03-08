384d05c218279c2bc724d8d65bb1cee3ddeb5023
andersonkaiti <anderkaiti@gmail.com>
2026-03-07 22:43:40 -0300

    :recycle: refactor(database): extract prisma client to db.ts and separate from auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,7 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { openAPI } from 'better-auth/plugins'
-import { env } from '../config/env.ts'
-import { PrismaClient } from '../generated/prisma/client.ts'
-
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: env.DATABASE_URL }),
-})
+import { prisma } from './db.ts'
 
 export const auth = betterAuth({
   trustedOrigins: ['http://localhost:3000'],
@@ -17,6 +11,5 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
-  // for better auth routes on swagger
   plugins: [openAPI()],
 })
---
+++ src/lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { env } from '../config/env.ts'
+import { PrismaClient } from '../generated/prisma/client.ts'
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL })
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
